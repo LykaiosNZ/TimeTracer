fbc266790985ce2a4303679649c7f4b36b4a03d0
Ben Simpson <ben@mrflux.net.nz>
2019-09-08 00:57:21 +1200

    Rename ConsoleTimeTrace => ConsoleTrace

--- src/TimeTracer/ConsoleTrace.cs
+++ src/TimeTracer/ConsoleTrace.cs
@@ -6,26 +6,26 @@ namespace TimeTracer
     /// <summary>
     /// A tracer that writes messages to the console.
     /// </summary>
-    public class ConsoleTimeTrace : TimeTrace
+    public class ConsoleTrace : TimeTrace
     {
         private readonly IMetricFormatter _metricFormatter;
         private readonly IScopeFormatter _scopeFormatter;
         private bool _disposed;
 
         /// <summary>
-        /// Initialise a new instance of the <see cref="ConsoleTimeTrace"/> class using the default formatters.
+        /// Initialise a new instance of the <see cref="ConsoleTrace"/> class using the default formatters.
         /// </summary>
-        public ConsoleTimeTrace()
+        public ConsoleTrace()
             : this(new DefaultMetricFormatter(), new DefaultScopeFormatter())
         {
         }
 
         /// <summary>
-        /// Initialise a new instance of the <see cref="ConsoleTimeTrace"/> class using specified formatters.
+        /// Initialise a new instance of the <see cref="ConsoleTrace"/> class using specified formatters.
         /// </summary>
         /// <param name="metricFormatter">Formatter to use for writing metrics. If null, metrics will not be written to the console.</param>
         /// <param name="scopeFormatter">Formatter to use for writing scope creation information. If null, scope creation/disposal </param>
-        public ConsoleTimeTrace(IMetricFormatter metricFormatter, IScopeFormatter scopeFormatter)
+        public ConsoleTrace(IMetricFormatter metricFormatter, IScopeFormatter scopeFormatter)
         {
             _metricFormatter = metricFormatter;
             _scopeFormatter = scopeFormatter;
